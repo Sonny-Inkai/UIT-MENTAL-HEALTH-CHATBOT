42ca9672fd5df961c5e0eb86f4d250ca8534563d
sonnyinkai <sonnyinkai@gmail.com>
2024-10-25 23:58:24 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
 import MainContent from './components/MainContent';
-import ChatIcon from './components/ChatICon';
+import ChatIcon from './components/ChatIcon';
 import './App.css';
 
 function App() {
