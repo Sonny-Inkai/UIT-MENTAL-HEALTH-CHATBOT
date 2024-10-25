708a403ea4273a7b1bcbdd2e229652d6ab317ca1
sonnyinkai <sonnyinkai@gmail.com>
2024-10-25 14:32:29 +0700

    init

--- src/model/generator.py
+++ src/model/generator.py
@@ -20,6 +20,7 @@ load_dotenv()
 CLOUD_ID = os.getenv("ELASTIC_CLOUD_ID")
 API_KEY = os.getenv("ELASTIC_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+HF_TOKENS = os.getenv("HF_TOKENS")
 
 # Tạo Flask App
 app = Flask(__name__)
@@ -40,8 +41,16 @@ text_embedder.warm_up()  # Ensure the model is loaded and ready
 # Bước 3: Khởi tạo Elasticsearch Embedding Retriever chỉ một lần khi ứng dụng được khởi động
 retriever = ElasticsearchEmbeddingRetriever(
     document_store=document_store,
-    top_k=1,
+    top_k=5,
 )
+
+ranker = TransformersSimilarityRanker(
+    model="namdp-ptit/ViRanker",
+    token=Secret.from_token(HF_TOKENS),
+    score_threshold=0.72,
+    top_k=2  # Chọn lọc ra 5 tài liệu phù hợp nhất sau khi rank
+)
+ranker.warm_up()
 
 # Bước 4: Setup ChatPromptBuilder chỉ một lần khi ứng dụng được khởi động
 prompt_builder = ChatPromptBuilder(template=[
@@ -53,7 +62,7 @@ prompt_builder = ChatPromptBuilder(template=[
     ),
     ChatMessage.from_user(
         "Dưới đây là một số thông tin tham khảo bao gồm các câu chuyện chữa lành, câu nói truyền động lực, và các tài liệu khác có thể hữu ích: \\n"
-        "{{ retriever_documents }} \\n\\n"
+        "{{ ranker_documents }} \\n\\n"
         "Tuy nhiên, hãy nhớ rằng các thông tin tham khảo này chỉ là hỗ trợ và không nhất thiết phải áp dụng cho mọi tình huống của sinh viên. "
         "Do đó, điều quan trọng là bạn cần ưu tiên lắng nghe và phản hồi trực tiếp dựa trên câu hỏi cụ thể của sinh viên. "
         "Hãy trả lời một cách rõ ràng, gần gũi, sử dụng các từ ngữ thân thiện, và tập trung vào đưa ra các lời khuyên cụ thể, súc tích nhưng mang tính hỗ trợ sâu sắc. "
@@ -67,18 +76,20 @@ prompt_builder = ChatPromptBuilder(template=[
 # Bước 5: Setup OpenAIChatGenerator chỉ một lần khi ứng dụng được khởi động
 chat_generator = OpenAIChatGenerator(
     api_key=Secret.from_env_var("OPENAI_API_KEY"),
-    model="gpt-4o"
+    model="gpt-4o-mini"
 )
 
 # Bước 6: Tạo và kết nối Pipeline chỉ một lần khi ứng dụng được khởi động
 pipeline = Pipeline()
 pipeline.add_component("text_embedder", text_embedder)
 pipeline.add_component("retriever", retriever)
+pipeline.add_component("ranker", ranker)
 pipeline.add_component("prompt_builder", prompt_builder)
 pipeline.add_component("llm", chat_generator)
 
 pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
-pipeline.connect("retriever.documents", "prompt_builder.retriever_documents")
+pipeline.connect("retriever.documents", "ranker.documents")
+pipeline.connect("ranker.documents", "prompt_builder.ranker_documents")
 pipeline.connect("prompt_builder.prompt", "llm.messages")
 
 # Bước 7: Tạo Endpoint cho API
@@ -92,6 +103,7 @@ def generate_response():
         # Chạy Pipeline với câu hỏi của người dùng
         response = pipeline.run({
             "text_embedder": {"text": user_question},
+            "ranker":{"query": user_question},
             "prompt_builder": {"user_question": user_question}
         })
 
@@ -102,4 +114,4 @@ def generate_response():
 
 # Chạy Flask App
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
--- src/model/generator3.py
+++ src/model/generator3.py
@@ -20,7 +20,6 @@ load_dotenv()
 CLOUD_ID = os.getenv("ELASTIC_CLOUD_ID")
 API_KEY = os.getenv("ELASTIC_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-HF_TOKENS = os.getenv("HF_TOKENS")
 
 # Tạo Flask App
 app = Flask(__name__)
@@ -41,40 +40,45 @@ text_embedder.warm_up()  # Ensure the model is loaded and ready
 # Bước 3: Khởi tạo Elasticsearch Embedding Retriever chỉ một lần khi ứng dụng được khởi động
 retriever = ElasticsearchEmbeddingRetriever(
     document_store=document_store,
-    top_k=5,
+    top_k=1,
 )
-
-ranker = TransformersSimilarityRanker(
-    model="namdp-ptit/ViRanker",
-    token=Secret.from_token(HF_TOKENS),
-    score_threshold=0.72,
-    top_k=2  # Chọn lọc ra 5 tài liệu phù hợp nhất sau khi rank
-)
-ranker.warm_up()
 
 # Bước 4: Setup ChatPromptBuilder chỉ một lần khi ứng dụng được khởi động
 prompt_builder = ChatPromptBuilder(template=[
-    ChatMessage.from_system("Bạn là một trợ lý tư vấn tâm lý thân thiện và hỗ trợ, dành cho học sinh sinh viên Việt Nam, đặc biệt là sinh viên Trường đại học công nghệ thông tin (UIT) Thành phố hồ chí minh. Nhiệm vụ của bạn là lắng nghe và đồng cảm với những chia sẻ của sinh viên, đồng thời cung cấp lời khuyên về sức khỏe tâm thần một cách nhẹ nhàng và chân thành. Hãy trả lời các câu hỏi bằng ngôn ngữ dễ hiểu, gần gũi, và đưa ra những gợi ý hữu ích, toàn diện, giúp sinh viên cảm thấy được thấu hiểu và có định hướng rõ ràng hơn trong việc cải thiện sức khỏe tâm lý của mình. Luôn nhớ rằng mục tiêu của bạn là mang lại sự an ủi và cảm giác an toàn cho sinh viên."),
-    ChatMessage.from_user("Các thông tin các câu truyện chữa lành, câu nói truyền động lực bạn có thể tham khảo ở đây:\\n{{ ranker_documents }} \\n\\n Tuy nhiên hãy nhớ rằng không phải lúc nào các thông tin tham khảo này cũng phù hợp với yêu cầu cua sinh viên, do đó bạn cần chú ý trả lời câu hỏi của sinh viên, và thông tin được cung cấp chỉ là hỗ trợ. Đây là câu hỏi của sinh viên: {{ user_question }}. Hãy trả lời dễ hiểu, gần gũi, đưa ra những gợi ý hữu ích, súc tích, và thân thiện nhất. \\n\\n Trả lời: "),
+    ChatMessage.from_system(
+        "Bạn là một trợ lý tư vấn tâm lý thân thiện và hỗ trợ, đặc biệt dành cho học sinh sinh viên Việt Nam, nhất là sinh viên Trường Đại học Công nghệ Thông tin (UIT) Thành phố Hồ Chí Minh. "
+        "Nhiệm vụ của bạn là lắng nghe, đồng cảm, và cung cấp lời khuyên về sức khỏe tâm thần một cách nhẹ nhàng, chân thành. "
+        "Bạn hãy sử dụng ngôn ngữ gần gũi, dễ hiểu và cố gắng đưa ra những gợi ý hữu ích, toàn diện, giúp sinh viên cảm thấy được thấu hiểu và có định hướng rõ ràng hơn trong việc cải thiện sức khỏe tâm lý của mình. "
+        "Luôn nhớ rằng mục tiêu chính của bạn là mang lại sự an ủi và cảm giác an toàn cho sinh viên."
+    ),
+    ChatMessage.from_user(
+        "Dưới đây là một số thông tin tham khảo bao gồm các câu chuyện chữa lành, câu nói truyền động lực, và các tài liệu khác có thể hữu ích: \\n"
+        "{{ retriever_documents }} \\n\\n"
+        "Tuy nhiên, hãy nhớ rằng các thông tin tham khảo này chỉ là hỗ trợ và không nhất thiết phải áp dụng cho mọi tình huống của sinh viên. "
+        "Do đó, điều quan trọng là bạn cần ưu tiên lắng nghe và phản hồi trực tiếp dựa trên câu hỏi cụ thể của sinh viên. "
+        "Hãy trả lời một cách rõ ràng, gần gũi, sử dụng các từ ngữ thân thiện, và tập trung vào đưa ra các lời khuyên cụ thể, súc tích nhưng mang tính hỗ trợ sâu sắc. "
+        "Luôn đảm bảo rằng câu trả lời của bạn là thân thiện, dễ tiếp cận, súc tích, ngắn gọn, và mang lại cảm giác an toàn cho sinh viên. "
+        "Đây là câu hỏi của sinh viên: {{ user_question }}. "
+        "\\n\\n "
+        "Trả lời: "
+    ),
 ])
 
 # Bước 5: Setup OpenAIChatGenerator chỉ một lần khi ứng dụng được khởi động
 chat_generator = OpenAIChatGenerator(
     api_key=Secret.from_env_var("OPENAI_API_KEY"),
-    model="gpt-4o-mini"
+    model="gpt-4o"
 )
 
 # Bước 6: Tạo và kết nối Pipeline chỉ một lần khi ứng dụng được khởi động
 pipeline = Pipeline()
 pipeline.add_component("text_embedder", text_embedder)
 pipeline.add_component("retriever", retriever)
-pipeline.add_component("ranker", ranker)
 pipeline.add_component("prompt_builder", prompt_builder)
 pipeline.add_component("llm", chat_generator)
 
 pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
-pipeline.connect("retriever.documents", "ranker.documents")
-pipeline.connect("ranker.documents", "prompt_builder.ranker_documents")
+pipeline.connect("retriever.documents", "prompt_builder.retriever_documents")
 pipeline.connect("prompt_builder.prompt", "llm.messages")
 
 # Bước 7: Tạo Endpoint cho API
@@ -88,7 +92,6 @@ def generate_response():
         # Chạy Pipeline với câu hỏi của người dùng
         response = pipeline.run({
             "text_embedder": {"text": user_question},
-            "ranker":{"query": user_question},
             "prompt_builder": {"user_question": user_question}
         })
 
@@ -99,4 +102,4 @@ def generate_response():
 
 # Chạy Flask App
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
